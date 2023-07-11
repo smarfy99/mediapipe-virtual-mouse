a8647ef33029ccab3ec6600d83a8688188c140ce
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2023-07-11 22:45:50 +0900

    [Feat] drawing utils handmark

--- virtualMouse2.py
+++ virtualMouse2.py
@@ -1,9 +1,22 @@
 import cv2
+import mediapipe as mp
 print(cv2.__version__)
 cap = cv2.VideoCapture(0)
+hand_detector = mp.solutions.hands.Hands()
+# hand의 landmark를 연결해서 시각화
+drawing_utils = mp.solutions.drawing_utils
 
+# cv2 라이브러리를 활용해 웹캠 불러오기
 while True:
     _, frame = cap.read()
+    # opencv는 BGR 형식(Blue Green Red)에서 작동하지만 mediapipe는 RGB(Red Green Blue) 형식에서 작동하기 때문에 이미지를 처리하기 전에 이미지 형식을 변경
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    output = hand_detector.process(rgb_frame)
+    hands = output.multi_hand_landmarks
+    print(hands)
+    if hands:
+        for hand in hands:
+            drawing_utils.draw_landmarks(frame, hand)
     cv2.imshow('Virtual Mouse', frame)
     cv2.waitKey(1)
     
