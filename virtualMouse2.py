808b15b2850c66bd6bf103e9059a98a02e685a1c
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2023-07-14 21:08:13 +0900

    [Feat] hand landmark 좌표 잡아주기

--- virtualMouse2.py
+++ virtualMouse2.py
@@ -9,6 +9,9 @@ drawing_utils = mp.solutions.drawing_utils
 # cv2 라이브러리를 활용해 웹캠 불러오기
 while True:
     _, frame = cap.read()
+    # 0는 x축, 1은 y축으로 flip
+    frame = cv2.flip(frame, 1)
+    frame_height, frame_width, _ = frame.shape
     # opencv는 BGR 형식(Blue Green Red)에서 작동하지만 mediapipe는 RGB(Red Green Blue) 형식에서 작동하기 때문에 이미지를 처리하기 전에 이미지 형식을 변경
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     output = hand_detector.process(rgb_frame)
@@ -17,6 +20,13 @@ while True:
     if hands:
         for hand in hands:
             drawing_utils.draw_landmarks(frame, hand)
+            landmarks = hand.landmark
+            for id, landmark in enumerate(landmarks):
+                # hand landmark
+                x = int(landmark.x * frame_width)
+                y = int(landmark.y * frame_height)
+                print(x,y)
+                    
     cv2.imshow('Virtual Mouse', frame)
     cv2.waitKey(1)
     
