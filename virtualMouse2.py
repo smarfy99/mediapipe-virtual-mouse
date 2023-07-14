cde2f7f09df945e7fe1a6dd07f544a4115187c41
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2023-07-14 21:24:11 +0900

    [Feat] hand landmark to mouse cursor

--- virtualMouse2.py
+++ virtualMouse2.py
@@ -1,10 +1,12 @@
 import cv2
 import mediapipe as mp
-print(cv2.__version__)
+import pyautogui
+
 cap = cv2.VideoCapture(0)
 hand_detector = mp.solutions.hands.Hands()
 # hand의 landmark를 연결해서 시각화
 drawing_utils = mp.solutions.drawing_utils
+screen_width, screen_height = pyautogui.size()
 
 # cv2 라이브러리를 활용해 웹캠 불러오기
 while True:
@@ -12,11 +14,12 @@ while True:
     # 0는 x축, 1은 y축으로 flip
     frame = cv2.flip(frame, 1)
     frame_height, frame_width, _ = frame.shape
+    
     # opencv는 BGR 형식(Blue Green Red)에서 작동하지만 mediapipe는 RGB(Red Green Blue) 형식에서 작동하기 때문에 이미지를 처리하기 전에 이미지 형식을 변경
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     output = hand_detector.process(rgb_frame)
     hands = output.multi_hand_landmarks
-    print(hands)
+    
     if hands:
         for hand in hands:
             drawing_utils.draw_landmarks(frame, hand)
@@ -28,6 +31,9 @@ while True:
                 print(x,y)
                 if id == 8:
                     cv2.circle(img=frame, center=(x,y), radius=10, color=(0,255,255))
+                    index_x = screen_width / frame_width * x
+                    index_y = screen_height / frame_height * y
+                    pyautogui.moveTo(index_x, index_y)
                     
     cv2.imshow('Virtual Mouse', frame)
     cv2.waitKey(1)
